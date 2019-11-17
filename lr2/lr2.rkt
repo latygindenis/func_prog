c584b5d939e30e98f803f5b96a0f06d7daad2552
Shabanova Daria <Shobik4456@gmail.com>
2019-11-17 22:26:41 +0700

    Check binary tree in order

---
+++ lr2/lr2.rkt
@@ -0,0 +1,48 @@
+#lang racket
+(define (power x n)
+  (cond ((= n 0) 1)
+  (else (* (power x (- n 1)) x )))
+)
+
+(define (copy_list list)
+  (cond ((null? list) '())
+  (else (cons (car list) (copy_list(cdr list)))))
+)
+
+(define (member element list)
+  (cond ((null? list) 'not_found)
+  ((= element (car list)) 'founded)
+  (else (member element (cdr list))))
+)
+
+(define (append list1 list2)
+  (cond ((null? list1) list2)
+  (else (cons (car list1) (append (cdr list1) list2))))
+)
+
+(define (reverse list)
+  (cond ((null? list) '())
+  (else (append (reverse (cdr list)) (cons (car list) '()))))
+)
+
+(define (full_copy_list list)
+  (cond ((null? list) '())
+  ((number? list) list) 
+  (else (cons (full_copy_list (car list)) (full_copy_list (cdr list))))))
+
+(define (node_counter tree)
+  (cond 
+    ((null? tree) 0)
+    (else(+ 1 (node_counter(second tree)) (node_counter(third tree))))))
+
+(define (is_binary_tree_order tree)
+  (cond
+    ((null? tree) #t)
+    ((and (null? (second tree)) (null? (third tree))) #t)
+    ((null? (second tree)) #t)
+    (( > (car (second tree)) (car tree)) #f)
+    ((null? (third tree)) #t)
+    (( < (car (third tree)) (car tree)) #f)
+    (else (and (is_binary_tree_order(second tree)) (is_binary_tree_order(third tree))))
+   )
+)
